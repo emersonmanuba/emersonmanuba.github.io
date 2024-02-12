3529c95d6cfe43aa12fb4045d6d874696c4b9303
emersonmanuba <ersonm.0802@gmail.com>
2024-02-13 00:01:48 +0800

    Tailwind creation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navigator from "./components/Navigator"
+
+function App() {
+ 
+  return (
+    <>
+      <Navigator />
+      <h1>  Testing Mother Fck</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navigator.tsx
@@ -0,0 +1,14 @@
+
+function Navigator() {
+ 
+    return (
+      <>
+        <nav className="navigator">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda corporis corrupti doloremque inventore, expedita rem maiores ducimus eaque sed rerum eum reiciendis dolor hic facilis nulla modi animi? Dignissimos, dolores!
+        </nav>
+      </>
+    )
+  }
+  
+  export default Navigator
+  
