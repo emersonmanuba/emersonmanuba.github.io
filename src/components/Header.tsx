e245a1e2dda809a1535b33aa6b66139083c9dbb9
emersonmanuba <ersonm.0802@gmail.com>
2024-09-21 10:34:33 +0300

    first phase of navbar done. animation and transform in next update. move on to Content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ function Header() {
  
     return (
       <>
-      <header className="relative overscroll-auto">        
+      <header className="relative overscroll-auto pt-16">        
       <img src="images/EMER_HEADER.png" alt="header_photo" />
       </header>
       
--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -3,39 +3,30 @@ function Navigator() {
  
     return (
       <>
-        <nav className="bg-gray-800 p-4 z-40">
+        <nav className="bg-gray-800 fixed top-0 w-full p-4 z-50">
           <div className="container mx-auto flex justify-between items-center">
             {/* Logo on the left side */}
             <div className="text-white text-xl font-bold">
-              <a href="/">Logo</a>
+              <a href="/"><img src="./white-hacker-male.svg" alt="male" className="size-10 hover:bg-gray-300 rounded p-1"/></a>
             </div>
 
             {/* Buttons on the right side */}
-            <div className="space-x-4">
-              <a href="#" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">
-                Button 1
+            <div className="space-x-2">
+              {/*<a href="#" className="text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-gray-300 hover:text-blue-900">
+                Home
+              </a>*/}
+              <a href="#" className="text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-gray-300 hover:text-blue-900">
+                About
               </a>
-              <a href="#" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">
-                Button 2
+              <a href="#" className="text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-gray-300 hover:text-blue-900">
+                Career
               </a>
-              <a href="#" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">
-                Button 3
-              </a>
-              <a href="#" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">
-                Button 4
+              <a href="#" className="text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-gray-300 hover:text-blue-900">
+                Contact
               </a>
             </div>
           </div>
         </nav>
-
-        {/*<nav className="nav_bar p-6 z-10 overflow-auto">
-            Navigator Bar 
-            <div className="buttons right-0">
-            <button className="p-2 bg-slate-500 hover:bg-slate-700 align-top"> about me! </button>
-            <button className="p-2 bg-slate-500 hover:bg-slate-700 align-super"> Contact me! </button>
-            </div>
-            
-        </nav>*/}
       </>
     )
   }
