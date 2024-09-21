05d297ca0c45dff6b81a3744ecbad79c75093f78
emersonmanuba <ersonm.0802@gmail.com>
2024-09-21 09:40:24 +0300

    update spacing.

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ function App() {
  
   return (
     <>
-
       <Navigator />
       <Header />
       <Content />
-
       <br /><br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda corporis corrupti doloremque inventore, expedita rem maiores ducimus eaque sed rerum eum reiciendis dolor hic facilis nulla modi animi? Dignissimos, dolores!
         
     </>
