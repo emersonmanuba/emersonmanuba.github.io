02590b94da8dd60bc97897f404d7758bc1824aad
emersonmanuba <ersonm.0802@gmail.com>
2025-12-09 01:01:33 +0300

    Initial

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center p-4 mt-10 border-t">
-      <p>© {new Date().getFullYear()} Emerson Manuba. All rights reserved.</p>
-    </footer>
-  );
-}
